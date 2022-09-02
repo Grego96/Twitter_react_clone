53c390b242fafd6d200d4c0ea2e63f978ce3ed05
Gregory <gregoryhunkeler@gmail.com>
2022-09-02 15:21:14 -0300

    base app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Profile from "./components/pages/Profile"
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Home() {
-
   useEffect(() => {
-    async function getHome(){
+    async function getHome() {
       const result = await axios({
         method: "get",
         baseURL: `http://localhost:${process.env.REACT_APP_API_PORT}`,
         headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN_TEST}` 
-        }
-        
-      })
+          Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN_TEST}`,
+        },
+      });
       console.log(result);
-      return result
+      return result;
     }
-    getHome()
-  }, [])
+    getHome();
+  }, []);
 
   return (
-    <div>Home</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-6"></div>
+        <div className="col-md-3"></div>
+      </div>
+    </div>
+  );
 }
