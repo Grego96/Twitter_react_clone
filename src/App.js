f6db27c527561c2c782f2e0a56e0a2f8040cfabd
Pablo Charamello <pablocharamello@gmail.com>
2022-09-02 12:43:49 -0300

    estructura base con rutas home y twitter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Tweets from "./pages/Tweets"
+// import AboutOurTwitter from "./pages/AboutOurTwitter";
 
-function App() {
+
+export const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tweets/:id" element={<Tweets />} />
+        {/* <Route path="*" element={<Error404 />} />         */}
+      </Routes>
   );
-}
-
-export default App;
+};
---
+++ src/pages/AboutOurTwitter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutOurTwitter() {
+  return (
+    <div>AboutOurTwitter</div>
+  )
+}
