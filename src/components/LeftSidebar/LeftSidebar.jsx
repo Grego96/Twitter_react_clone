bf349773d5de98d8a5fc969e4e241590ee9d7990
Pablo Charamello <pablocharamello@gmail.com>
2022-09-04 17:12:58 -0300

    sidebars home retouch

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -12,43 +12,61 @@ import bookmarks from "./svg/bookmarks_icon.svg";
 import listIcon from "./svg/listIicon.svg";
 export default function LeftSidebar() {
   return (
-    
-      <div className="sidebar-container">
+    <div className="sidebar-container">
+      <Link to="/">
         <img className="twitter-icon" src={twitter_logo} alt="" />
-        <Link to="/" className="sidebar-left-links home-sidebar">
-          <img className="birdhouse" src={birdhouse} alt="" />
-          Home
-        </Link>
-        <Link to="#" className="sidebar-left-links">
+      </Link>
+      <Link to="/" className="home-sidebar">
+        <div>
+        <span >
+          <div>
+          <img className="birdhouse" src={birdhouse} alt="" /><sup className="birdhouse-dot">·</sup>Home</div>          
+        </span>
+        </div>
+      </Link>
+      <Link to="#" className="explore">
+        <span className="hashtag">          
           <img className="birdhouse" src={hashtag} alt="" />
           Explore
-        </Link>
-        <Link to="#" className="sidebar-left-links">
-          <img className="bell" src={bell} alt="" />
-          Notifications
-        </Link>
-        <Link to="#" className="sidebar-left-links-notification sidebar-left-links">
-          <img className="bell" src={message} alt="" />
-          Notifications
-        </Link>
-        <Link to="#" className="sidebar-left-links">
-          <img className="bell" src={bookmarks} alt="" />
-          Bookmarks
-        </Link>
-        <Link to="#" className="sidebar-left-links">
-          <img className="bell" src={listIcon} alt="" />
-          Lists
-        </Link>
-        <Link to="#" className="sidebar-left-links">
-          <img className="bell" src={profile} alt="" />
-          Profile
-        </Link>
-        <Link to="#" className="sidebar-left-links">
-          <img className="bell" src={threePoints} alt="" />
-          More
-        </Link>
-        <button className="btn-tweet">Tweet</button>
-      </div>
-    
+        </span>
+      </Link>
+      <Link to="#" className="sidebar-left-links-notification">
+        <span ><div className="">
+          <img className="bell" src={bell} alt="" /><sup className="notifications-count">5</sup>Notifications
+          </div>      
+        </span>
+      </Link>
+      <Link to="#"  className="messages">
+        <span  className=" sidebar-left-links">
+          <img className="messages-icon" src={message} alt="" />
+          Messages
+        </span>
+      </Link>
+      <Link to="#" className=" bookmarks">
+        <span className="sidebar-left-links">
+        <img className="bookmarks-icon" src={bookmarks} alt="" />
+        Bookmarks
+        </span>
+      </Link>
+      <Link to="#" className="list">
+        <span className="sidebar-left-links">
+        <img className="list-icon" src={listIcon} alt="" />
+        Lists
+        </span>
+      </Link>
+      <Link to="#" className="profile">
+        <span className="sidebar-left-links">
+        <img className="profile-icon" src={profile} alt="" />
+        Profile
+        </span>
+      </Link>
+      <Link to="#" className="more">
+        <span className="sidebar-left-links">
+        <img className="more-icon" src={threePoints} alt="" />
+        More
+        </span>
+      </Link>
+      <button className="btn-tweet">Tweet</button>
+    </div>
   );
 }
--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import "./RightSidebar.css";
 import searchIcon from "./svg/searchIcon.svg";
 import profileWhoToFollow from "./img/profileWhoToFollow.jpg";
+import profileWhoToFollow2 from "./img/profileWhoToFollow2.jpg";
+import profileWhoToFollow3 from "./img/profileWhoToFollow3.jpg";
 import threeDots from "./svg/ThreeDots.svg";
 
 export default function RightSidebar() {
@@ -48,6 +50,22 @@ export default function RightSidebar() {
         <p className="who-to-follow">Who to follow</p>
         <div className="d-flex profile-container">
           <img className="img-who-to-follow" src={profileWhoToFollow} alt="" />
+          <Link className="profile-nick" to="#">
+            profileNick
+          </Link>
+          <span className="profile-name">@profileName</span>
+          <button className="follow-buton">Follow</button>
+        </div>        
+        <div className="d-flex profile-container">
+          <img className="img-who-to-follow" src={profileWhoToFollow2} alt="" />
+          <Link className="profile-nick" to="#">
+            profileNick
+          </Link>
+          <span className="profile-name">@profileName</span>
+          <button className="follow-buton">Follow</button>
+        </div>
+        <div className="d-flex profile-container">
+          <img className="img-who-to-follow" src={profileWhoToFollow3} alt="" />
           <Link className="profile-nick" to="#">
             profileNick
           </Link>
